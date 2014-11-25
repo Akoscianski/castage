a1327c2092c170618e02f10ecfe6340d70f60829
Emmanuel608 <em.galet@yahoo.fr>
2014-11-25 14:30:21 +0100

    Changement des fonctions php pour utiliser MySQL
    Modifications des fonctions permettant la connexion, la récupération des
    tuples dans la BD, la modification de ces tuples dans des tableaux, ...

--- php/ajax/btn_notif.php
+++ php/ajax/btn_notif.php
@@ -1,11 +1,10 @@
 <?php
 	include '../session.inc'; check_login();
 	
-	$db= mysql_connect('localhost','castage','castage');
-	mysql_select_db('castage',$db);
+	$db= connect();
 	$sql="SELECT COUNT(IdNotif) FROM notif WHERE IdUser = ".$_SESSION['id']." AND Vu = 0;";
-	$req=mysql_query($sql);
-	$retour=mysql_fetch_array($req, MYSQL_BOTH);
+	$req=mysqli_query($db,$sql);
+	$retour=mysqli_fetch_array($req, MYSQL_BOTH);
 	echo $retour[0]." Notifications";
 	//echo rand(1,100).' Notifications';
 ?>
